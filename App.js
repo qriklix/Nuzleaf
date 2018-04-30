a0e55abd4b52dce8a8ab5b936521607301437f1d
Peter <peter.fabian@pintofsource.com>
2018-05-01 00:20:35 +0200

    Add: redux / react-navigation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Hello world!!!</Text>
+        </View>
+      </Provider>
     );
   }
 }
